0eda775bb225bd6751cd4527c2d7b449f140778a
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-03-19 21:28:01 +0600

    return the next number that integer passed

---
+++ Very_Easy/problem3.js
@@ -0,0 +1,10 @@
+// Return the next number from integer passed
+
+function nextNum(number) {
+    var num = parseInt(number);
+    return num + 1;
+}
+
+var nextNumber = nextNum(4);
+
+console.log(nextNumber);
--- Very_Easy/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-// Convert minutes into seconds
-
-function minToSec(minutes) {
-    var convertMins = minutes * 60;
-    return convertMins;
-}
-
-var seconds = minToSec(3);
-console.log('Minute to seconds is: ', seconds);
