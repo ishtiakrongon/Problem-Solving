984f40f996873a144d56744dc346cd54edd3569d
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-03-20 23:34:25 +0600

    updated

--- Very_Easy/problem4.js
+++ Very_Easy/problem4.js
@@ -1,9 +1,9 @@
 // Area of a triangle
 
-function areaTriangle (base, height) {
+ function areaTriangle (base, height) {
     var areaTriag = (base * height) / 2;
-    return areaTriag;
-}
+     return areaTriag;
+ }
 
-var result = areaTriangle(4, 2.5);
-console.log(result);
+ var result = areaTriangle(4, 2.5);
+ console.log(result);
--- Very_Easy/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-// Area of a triangle
-
-function areaTriangle (base, height) {
-    var areaTriag = (base * height) / 2;
-    return areaTriag;
-}
-
-var result = areaTriangle(4, 2.5);
-console.log(result);
